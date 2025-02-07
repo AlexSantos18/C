8b6a69583295395091e581779a54f7442426325f
AlexSantos18 <ass.santos0@gmail.com>
2025-02-07 16:52:44 -0300

    Atualizaçao do Cabeçalho com Função

---
+++ CartasSuperTrunfo.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+void imprimir_cab(void){
+
+    printf("=======================================\n");
+    printf("= Cadastramento da Carta Super Trunfo =\n");
+    printf("=======================================\n");
+
+}
+
+void imprimir_dados(void){
+
+    
+    printf("=======================\n");
+    printf("=  Dados Cadastrados  =\n");
+    printf("=======================\n");
+}
+
+
+int main() {
+ 
+    char estado[2];
+    char cod_carta[5];
+    char cidade[50];
+    int populacao = 0;
+    float area = 0;
+    float pib = 0;
+    int n_turisticos = 0;
+    
+    // Inicio da solicitaçao de dados dos usuarios
+    imprimir_cab();
+    printf("Digite a letra correspondente ao Estado: ");
+    scanf(" %s", estado);
+    printf("Digite o Codigo da Carta: ");
+    scanf(" %s", cod_carta);
+    printf("Digite a Cidade: ");
+    scanf(" %s", cidade);
+    printf("Digite a Populacao: ");
+    scanf("%d", &populacao);
+    printf("Digite a Area: ");
+    scanf("%f", &area);
+    printf("Digite o PIB: ");
+    scanf("%f", &pib);
+    printf("Digite os Numeros de Pontos Turisticos: ");
+    scanf("%d", &n_turisticos);
+    printf("Carta Cadastrada com Sucesso.\n");
+    printf("\n");
+    printf("\n");
+    getch();
+   
+
+    // Impressao dos dados digitados pelo usuario
+
+    imprimir_dados();
+    printf("Estado: %s\n", estado);
+    printf("Codigo da Carta: %s\n", cod_carta);
+    printf("Nome da Cidade: %s\n", cidade);
+    printf("Populacao: %d\n", populacao);
+    printf("Area: %.2f km\n", area);
+    printf("PIB: %.2f bilhoes de reais\n", pib);
+    printf("Numero de Pontos Turisticos: %d\n", n_turisticos);
+    printf("\n");
+    printf("\n");
+    getch();
+
+    return 0;
+}
