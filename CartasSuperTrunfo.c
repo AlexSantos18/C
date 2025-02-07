ac281259afcde13ef982280b01a9e477e4e4dcca
AlexSantos18 <ass.santos0@gmail.com>
2025-02-07 18:38:45 -0300

    Testando uma nova versao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+char estado[2];
+char cod_carta[5];
+char cidade[50];
+int populacao = 0;
+float area = 0;
+float pib = 0;
+int n_turisticos = 0;
+
 void imprimir_cab(void){
 
     printf("=======================================\n");
@@ -9,26 +17,14 @@ void imprimir_cab(void){
 }
 
 void imprimir_dados(void){
-
     
-    printf("=======================\n");
-    printf("=  Dados Cadastrados  =\n");
-    printf("=======================\n");
+    printf("========================================\n");
+    printf("=  Cadastrados das Carta Super Trunfo  =\n");
+    printf("========================================\n");
 }
 
+void cadastro(void){
 
-int main() {
- 
-    char estado[2];
-    char cod_carta[5];
-    char cidade[50];
-    int populacao = 0;
-    float area = 0;
-    float pib = 0;
-    int n_turisticos = 0;
-    
-    // Inicio da solicitaçao de dados dos usuarios
-    imprimir_cab();
     printf("Digite a letra correspondente ao Estado: ");
     scanf(" %s", estado);
     printf("Digite o Codigo da Carta: ");
@@ -44,14 +40,10 @@ int main() {
     printf("Digite os Numeros de Pontos Turisticos: ");
     scanf("%d", &n_turisticos);
     printf("Carta Cadastrada com Sucesso.\n");
-    printf("\n");
-    printf("\n");
-    getch();
-   
+}
 
-    // Impressao dos dados digitados pelo usuario
-
-    imprimir_dados();
+void Lista_cad(void){
+    
     printf("Estado: %s\n", estado);
     printf("Codigo da Carta: %s\n", cod_carta);
     printf("Nome da Cidade: %s\n", cidade);
@@ -59,6 +51,24 @@ int main() {
     printf("Area: %.2f km\n", area);
     printf("PIB: %.2f bilhoes de reais\n", pib);
     printf("Numero de Pontos Turisticos: %d\n", n_turisticos);
+
+}
+
+
+int main() {
+ 
+
+    // Inicio da solicitaçao de dados dos usuarios
+    imprimir_cab();
+    cadastro();
+    printf("\n");
+    printf("\n");
+    getch();
+ 
+    // Impressao dos dados digitados pelo usuario
+
+    imprimir_dados();
+    Lista_cad();
     printf("\n");
     printf("\n");
     getch();
