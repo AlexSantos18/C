74bf74e940177ffd605d6471bafba15c65a0429a
Alex Sandro dos Santos <158177491+AlexSantos18@users.noreply.github.com>
2025-09-30 22:22:14 -0300

    update

--- DesafioWarsAventureiro.c
+++ DesafioWarsAventureiro.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 // Constantes Globais
 #define MAX_TERRITORIO 5
@@ -8,67 +9,76 @@
 #define TAM_STRING_COR 10
 
 // Estrutura de dados
-typedef struct {
+typedef struct
+{
     char nome[TAM_STRING_NOME];
     char cor[TAM_STRING_COR];
-    int tropas;   
+    int tropas;
 } Territorio;
 
 // Função limpa buffter do teclado
-void LimpaBuffer() {
+void LimpaBuffer()
+{
     int c;
-    while ((c = getchar()) != '\n' && c != EOF);
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
 }
 
 // Função para cadastrar um território
-int CadastrarTerritorio(Territorio *continente, int totalcontinente) {
-    if (totalcontinente >= MAX_TERRITORIO) {
+int CadastrarTerritorio(Territorio *continente, int totalcontinente)
+{
+    if (totalcontinente >= MAX_TERRITORIO)
+    {
         printf("\n**** Limite maximo de territorios atingido! ****\n\n");
         return totalcontinente;
     }
-    
+
     printf("\n########################\n");
     printf("Cadastro Novo Territorio\n");
     printf("########################\n\n");
-    
+
     //  equivalente a (continente + totalcontinente) utilizado no desafio novato
     Territorio *novoTerritorio = &continente[totalcontinente];
-    
+
     printf("Digite o nome do Territorio %d: ", totalcontinente + 1);
     fgets(novoTerritorio->nome, TAM_STRING_NOME, stdin);
     novoTerritorio->nome[strcspn(novoTerritorio->nome, "\n")] = '\0';
-    
+
     printf("Digite a cor das tropas (Ex: Verde, Vermelho...): ");
     fgets(novoTerritorio->cor, TAM_STRING_COR, stdin);
     novoTerritorio->cor[strcspn(novoTerritorio->cor, "\n")] = '\0';
-    
+
     printf("Digite a quantidade de tropas: ");
-    while (scanf("%d", &novoTerritorio->tropas) != 1 || novoTerritorio->tropas < 0) {
+    while (scanf("%d", &novoTerritorio->tropas) != 1 || novoTerritorio->tropas < 0)
+    {
         printf("Valor invalido! Digite um numero positivo: ");
         LimpaBuffer();
     }
     LimpaBuffer();
-    
+
     printf("\nTerritorio cadastrado com sucesso!\n");
     printf("__________________________________\n\n");
-    
+
     return totalcontinente + 1;
 }
 
 // Função para listar territórios
-void ListarTerritorios(Territorio *continente, int totalcontinente) {
+void ListarTerritorios(Territorio *continente, int totalcontinente)
+{
     printf("\n########################\n");
     printf("Listagem dos Territorios\n");
     printf("########################\n\n");
-    
-    if (totalcontinente == 0) {
+
+    if (totalcontinente == 0)
+    {
         printf("**** Nao existe Territorio Cadastrado! ****\n\n");
         return;
     }
-    
+
     // Usando ponteiros
-    for (int i = 0; i < totalcontinente; i++) {
-        Territorio *atual = continente + i;  // Equivalente a &continente[i]
+    for (int i = 0; i < totalcontinente; i++)
+    {
+        Territorio *atual = continente + i; // Equivalente a &continente[i]
         printf("Territorio %d:\n", i + 1);
         printf("  Nome: %s\n", atual->nome);
         printf("  Cor das Tropas: %s\n", atual->cor);
@@ -79,77 +89,153 @@ void ListarTerritorios(Territorio *continente, int totalcontinente) {
 
 // Função para atacar territórios
 
+void Batalha(Territorio *continente, int totalcontinente)
+{
+    int ataque, defesa;
+    srand(time(NULL));
 
+    printf("\n########################\n");
+    printf("Vamos Batalhar\n");
+    printf("########################\n\n");
 
+    if (totalcontinente < 5)
+    {
+        printf("**** Nao existe Territorio suficiente Cadastrado para a batalha! ****\n\n");
+        return;
+    }
 
+    // impressao para a escolha
 
+    for (int i = 0; i < totalcontinente; i++)
+    {
+        Territorio *atual = continente + i;
+        printf("Territorio %d: , Nome: %s, Cor: %s, Numeros de tropas: %d\n", i + 1, atual->nome, atual->cor, atual->tropas);
+        printf("________________________________________________________________________\n\n");
+    }
 
+    printf("\n\n");
+    printf("Escolha o numero do Territorio de Ataque: ");
+    if (scanf("%d", &ataque) != 1)
+    {
+        printf("\nOpcao invalida! Digite um numero entre 1 e 5\n");
+        LimpaBuffer();
+    }
+    printf("Escolha o numero do Territorio que Defendera: ");
+    if (scanf("%d", &defesa) != 1)
+    {
+        printf("\nOpcao invalida! Digite um numero entre 1 e 5\n");
+        LimpaBuffer();
+    }
+    printf("Rolando o dado .....\n");
+    int dado_ataque = 1 + rand() % 10;
+    int dado_defesa = 1 + rand() % 10;
 
+    printf("O Territorio %d Atque tirou %d\n", ataque, dado_ataque);
+    printf("O Territorio %d Defesa tirou %d\n", defesa, dado_defesa);
+    printf("________________________________________________________________________\n\n");
 
+    if (dado_ataque == dado_defesa)
+    {
+        printf("Houve empate!!");
+    }
+    else if (dado_ataque > dado_defesa)
+    {
+        printf("O Territorio de Ataque Ganhou!!!\n");
+        (continente + (defesa - 1))->tropas = (continente + (defesa - 1))->tropas - 1;
+        if ((continente + (defesa - 1))->tropas == 0)
+        {
+            printf("O Territorio de Ataque dominou o Territorio de Defesa!!!\n");
+            strcpy((continente + (defesa - 1))->cor, (continente + (ataque - 1))->cor);
+            (continente + (defesa - 1))->tropas = 1;
+        }
+    }
+    else
+    {
+        printf("O Territorio de Defesa Ganhou!!!\n");
+        (continente + (ataque - 1))->tropas = (continente + (ataque - 1))->tropas - 1;
+        if ((continente + (ataque - 1))->tropas == 0)
+        {
+            printf("O Territorio de Defesa dominou o Territorio de Ataque!!!\n");
+            strcpy((continente + (ataque - 1))->cor, (continente + (defesa - 1))->cor);
+            (continente + (ataque - 1))->tropas = 1;
+        }
+    }
+    printf("\n\n");
+    for (int i = 0; i < totalcontinente; i++)
+    {
+        Territorio *atual = continente + i;
+        printf("Territorio %d: , Nome: %s, Cor: %s, Numeros de tropas: %d\n", i + 1, atual->nome, atual->cor, atual->tropas);
+        printf("________________________________________________________________________\n\n");
+    }
+}
 
 // Programa principal
-int main() {
+int main()
+{
     Territorio *continente = NULL;
     int totalcontinente = 0;
     int opcao;
-    
+
     // Alocando memória com calloc (inicializa com zeros)
-    continente = (Territorio *) calloc(MAX_TERRITORIO, sizeof(Territorio));
-    
-    if (continente == NULL) {
+    continente = (Territorio *)calloc(MAX_TERRITORIO, sizeof(Territorio));
+
+    if (continente == NULL)
+    {
         fprintf(stderr, "Erro: Falha ao alocar memoria para continente!\n");
         return EXIT_FAILURE;
     }
-    
+
     // Menu de Cadastro
-    do {
+    do
+    {
         printf("===========================\n");
         printf("Cadastro de Territorios\n");
         printf("===========================\n");
         printf("1 - Cadastrar um novo Territorio\n");
         printf("2 - Listagem de Territorios\n");
-        printf("3 - Sair\n");
+        printf("3 - Batalha\n");
+        printf("4 - Sair\n");
         printf("___________________________\n");
         printf("Escolha uma opcao: ");
-        
+
         // Ler a opção do usuário com validação
-        if (scanf("%d", &opcao) != 1) {
+        if (scanf("%d", &opcao) != 1)
+        {
             printf("\nOpcao invalida! Digite um numero.\n\n");
             LimpaBuffer();
             continue;
         }
         LimpaBuffer();
-        
+
         // Opções no switch
-        switch (opcao) {
-            case 1:
-                totalcontinente = CadastrarTerritorio(continente, totalcontinente);
-                break;
-                
-            case 2:
-                ListarTerritorios(continente, totalcontinente);
-                break;
-                
-            case 3:
-                printf("\nEncerrando o programa...\n");
-                break;
-                
-            default:
-                printf("\nOpcao invalida! Escolha entre 1 e 3.\n\n");
-                break;
+        switch (opcao)
+        {
+        case 1:
+            totalcontinente = CadastrarTerritorio(continente, totalcontinente);
+            break;
+
+        case 2:
+            ListarTerritorios(continente, totalcontinente);
+            break;
+
+        case 3:
+            Batalha(continente, totalcontinente);
+            break;
+
+        case 4:
+            printf("\nEncerrando o programa...\n");
+            break;
+
+        default:
+            printf("\nOpcao invalida! Escolha entre 1 e 4.\n\n");
+            break;
         }
-        
-    } while (opcao != 3);
-    
+
+    } while (opcao != 4);
+
     // Liberando memória alocada
     free(continente);
-    continente = NULL;  
-    
+    continente = NULL;
+
     return EXIT_SUCCESS;
 }
-
-
-
-
-
-
